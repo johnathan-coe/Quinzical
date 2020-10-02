2fe3f921cc5fdbb490c2c41c699a12066fe6a2f3
Hiruna Jayamanne <hiru@hiru.dev>
2020-10-02 21:33:34 +1300

    Add run script
    This script will check for a jar file to run and if the javafx path exists.

---
+++ run.sh
@@ -0,0 +1,21 @@
+if [[ -z $JAR ]]; then
+  JAR=jeopardy.jar
+fi
+
+if [[ -z $JAVAFX_PATH ]]; then
+  JAVAFX_PATH=~/javafx-sdk-11.0.2/lib
+fi
+
+# Check for the Jeopardy jar
+if [[ ! -e "$JAR" ]]; then
+  echo "No \`$JAR\` file could be found." >& 2
+  exit 1
+fi
+
+# Check for JavaFX 11.0.2
+if [[ ! -d "$JAVAFX_PATH" ]]; then
+  echo "JavaFX 11.0.2 is not installed at the \`$JAVAFX_PATH\`." >&2
+  exit 1
+fi
+
+java --module-path $JAVAFX_PATH --add-modules javafx.controls,javafx.media,javafx.base,javafx.fxml -jar $JAR

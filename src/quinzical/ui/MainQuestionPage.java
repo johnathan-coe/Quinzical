446bfc45a312c4968a7ccbcd9091b3fc3284a4be
Hiruna Jayamanne <hiru@hiru.dev>
2020-10-17 05:12:26 +1300

    Add a timer that causes the game to time out
    If the user does not answer the question in 30 seconds, the game will take it as an "incorrect" answer and take you to the result screen.

--- src/quinzical/ui/MainQuestionPage.java
+++ src/quinzical/ui/MainQuestionPage.java
@@ -1,5 +1,7 @@
 package quinzical.ui;
 
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import quinzical.Game;
@@ -7,8 +9,13 @@ import quinzical.data.Question;
 import quinzical.festival.Festival;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainQuestionPage extends QuestionPage {
+	private Timer timer;
+	private final static int TIME_LIMIT = 30; // 30 seconds
+
 	public MainQuestionPage(Game game, Stage stage, Festival festival) throws IOException {
 		super(game, stage, festival, game.selectPage());
 
@@ -23,19 +30,43 @@ public class MainQuestionPage extends QuestionPage {
 		question.setState((correct)? Question.QuestionState.CORRECT: Question.QuestionState.INCORRECT);
 		int score = game.data().score() + (question.value() * ((correct)? 1: 0));
 		game.data().setScore(score);
-		dialog.show(question);
+		showDialog();
 	}
 
 
 	@Override
 	public void dontKnowPressed() {
 		question.setState(Question.QuestionState.INCORRECT);
+		showDialog();
+	}
+
+	private void showDialog() {
 		dialog.show(question);
+		timer.cancel();
 	}
 
 	@Override
 	public void show(Question question, String cat) {
 		super.show(question, cat);
 		question.setState(Question.QuestionState.INCORRECT);
+
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+			int n = TIME_LIMIT;
+
+			@Override
+			public void run() {
+				if (n <= 0) {
+					Platform.runLater(new Task<Void>() {
+						@Override
+						protected Void call() {
+							dontKnowPressed();
+							return null;
+						}
+					});
+				}
+				n--;
+			}
+		}, 0, 1000);
 	}
 }

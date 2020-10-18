b308b93c4520c67d7fd53fc0aa50caef3b57c461
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2020-10-18 23:08:51 +1300

    Add "Incorrect" category in practice mode for questions the user got incorrect

--- src/main/java/quinzical/ui/PracticeSelectPage.java
+++ src/main/java/quinzical/ui/PracticeSelectPage.java
@@ -1,8 +1,9 @@
 package quinzical.ui;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -13,15 +14,19 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import quinzical.Game;
+import quinzical.data.Category;
 import quinzical.data.CategoryParser;
 import quinzical.data.Question;
 
 public class PracticeSelectPage extends SelectPage {
 	private CategoryParser parser;
+	private Random rand;
 	@FXML Label header;
 	
 	public PracticeSelectPage(Game game, Stage stage) throws IOException {
 		super(game, stage);
+		
+		rand = new Random();
 		
 		// Alter the default text
 		header.setText("Select a category");
@@ -39,6 +44,26 @@ public class PracticeSelectPage extends SelectPage {
 	    parser = game.data().parser();
 		if (parser == null) { return; }
 		
+		// Category for questions the user got incorrect
+		if (getIncorrect().size() > 0) {
+			Pane incorrect;
+			
+			try { incorrect = new Card("Incorrect").pane();
+			} catch (IOException e) { e.printStackTrace(); return; }
+			
+			incorrect.setOnMouseClicked(
+				new EventHandler<MouseEvent>() {
+					@Override
+					public void handle(MouseEvent e) {
+							showIncorrect();
+					}
+				}
+			);
+			
+			container.getChildren().add(incorrect);
+		}
+		
+		// All categories
 		for (String cat : parser.categories()) {
 			try {
 				Pane p = new Card(cat).pane();
@@ -49,9 +74,7 @@ public class PracticeSelectPage extends SelectPage {
 					}
 				});
 				container.getChildren().add(p);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+			} catch (IOException e) { e.printStackTrace(); }
 		}
 	}
 	
@@ -61,18 +84,59 @@ public class PracticeSelectPage extends SelectPage {
 	}
 	
 	/**
+	 * Gets a list of all questions the user got incorrect
+	 */
+	private List<Question> getIncorrect() {
+		List<Question> incorrect = new ArrayList<Question>();
+		
+		// For all loaded categories
+		for (Category cat : game.data().categories()) {
+			// For all questions in the cat
+			for (Question q : cat.questions()) {
+				// If we got it incorrect
+				if (q.state() == Question.QuestionState.INCORRECT) {
+					// Add to list, cloning as to not alter main game state
+					incorrect.add(
+						new Question(0, q.prompt(), q.question(), q.answer(), Question.QuestionState.INCORRECT)
+					);
+				}
+			}
+		}
+		
+		return incorrect;
+	}
+	
+	// Random object from collection
+	private Object randomElement(Collection<?> l) {
+		Object[] arr = l.toArray();
+		return arr[rand.nextInt(arr.length)];
+	}
+	
+	/**
+	 * Display a randomly selected incorrect question
+	 */
+	private void showIncorrect() {
+		// Get all incorrect questions
+		List<Question> incorrect = getIncorrect();
+		
+		// Select a random question
+		Question randomQ = (Question) randomElement(incorrect);
+		
+		// Display
+		game.practiceQuestionPage().show(randomQ, "Incorrect");
+	}
+	
+	/**
 	 * Display a randomly selected question from the chosen category
 	 *
 	 * @param c Category object
 	 */
 	private void showQuestion(String c) {
-		Random rand = new Random();
-	
 		// Get all questions in the category
 		Map<String, String[]> questions = parser.getCategory(c);
+		
 		// Select a random question
-		Object[] qArray = questions.keySet().toArray();
-		String randomQ = (String) qArray[rand.nextInt(qArray.length)];
+		String randomQ = (String) randomElement(questions.keySet());
 		
 		// Create a question object
 		Question q = new Question(0, questions.get(randomQ)[0], randomQ, questions.get(randomQ)[1], Question.QuestionState.UNATTEMPTED);

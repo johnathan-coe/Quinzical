1651da5dce2f3419382e69d92ba7e52fa8a04787
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2020-10-25 07:31:06 +1300

    Move HelpViewer link plumbing out of the main class

--- src/main/java/quinzical/ui/help/HelpViewer.java
+++ src/main/java/quinzical/ui/help/HelpViewer.java
@@ -1,28 +1,13 @@
 package quinzical.ui.help;
 
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
-import javafx.concurrent.Worker;
-import javafx.concurrent.Worker.State;
 import javafx.fxml.FXML;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import quinzical.Game;
 
-import org.commonmark.node.AbstractVisitor;
-import org.commonmark.node.Image;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.events.EventTarget;
-import org.w3c.dom.events.Event;
-
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-import org.apache.commons.text.WordUtils;
-
+import quinzical.Game;
 import quinzical.ui.scenes.Page;
 
 /**
@@ -38,10 +23,11 @@ public class HelpViewer extends Page {
 		// Place this helpviewer page on a new stage
 		super(game, new Stage(), "/fxml/help-viewer.fxml");
 		
+		// Markdown renderer
 		md = new Markdown();
 		
-		// When a page is loaded, ensure all links render markdown
-		view.getEngine().getLoadWorker().stateProperty().addListener(new loadListener());
+		// Plumb all links on the webview to the Markdown viewer
+		new LinkPlumber(this, view);
 	}
 	
 	/**
@@ -70,41 +56,5 @@ public class HelpViewer extends Page {
 	// Show a heading in the current file
 	public void showHeading(String h) {
 		show(file, h);
-	}
-	
-		 
-	// On page load, make all links render markdown when clicked
-	private class loadListener implements ChangeListener<State> {
-		// Listens to a link click
-		private class onClick implements org.w3c.dom.events.EventListener {
-			@Override
-			public void handleEvent(Event ev) {
-				// Convert link content to a heading
-				String heading = ev.getTarget().toString().substring(1);
-				heading = heading.replace("-", " ");
-				heading = WordUtils.capitalizeFully(heading);
-				final String h = heading;
-				
-				Platform.runLater(new Task<Void>() {
-					@Override
-					protected Void call() {
-						showHeading(h);
-						return null;
-					}
-				});
-			}
-		 };
-		
-		@Override
-		public void changed(ObservableValue<? extends State> ov, State oldState, State newState) {
-			if (newState == Worker.State.SUCCEEDED) {
-				 org.w3c.dom.Document doc = view.getEngine().getDocument();
-				 NodeList links = doc.getElementsByTagName("a");
-
-				 for (int link = 0; link < links.getLength(); link++) {
-					((EventTarget) links.item(link)).addEventListener("click", new onClick(), false);
-				 }
-			}
-		}
 	}
 }
---
+++ src/main/java/quinzical/ui/help/LinkPlumber.java
@@ -0,0 +1,59 @@
+package quinzical.ui.help;
+
+import org.apache.commons.text.WordUtils;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventTarget;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.concurrent.Worker.State;
+import javafx.scene.web.WebView;
+
+public class LinkPlumber implements ChangeListener<State> {
+		HelpViewer viewer;
+		WebView view;
+		
+		public LinkPlumber(HelpViewer viewer, WebView view) {
+			this.viewer = viewer;
+			this.view = view;
+			
+			// Add this plumber to the document
+			view.getEngine().getLoadWorker().stateProperty().addListener(this);
+		}
+	
+		// Listens to a link click
+		private class onClick implements org.w3c.dom.events.EventListener {
+			@Override
+			public void handleEvent(Event ev) {
+				// Convert link content to a heading
+				String heading = ev.getTarget().toString().substring(1);
+				heading = heading.replace("-", " ");
+				heading = WordUtils.capitalizeFully(heading);
+				final String h = heading;
+				
+				Platform.runLater(new Task<Void>() {
+					@Override
+					protected Void call() {
+						viewer.showHeading(h);
+						return null;
+					}
+				});
+			}
+		 };
+		
+		@Override
+		public void changed(ObservableValue<? extends State> ov, State oldState, State newState) {
+			if (newState == Worker.State.SUCCEEDED) {
+				 org.w3c.dom.Document doc = view.getEngine().getDocument();
+				 NodeList links = doc.getElementsByTagName("a");
+
+				 for (int link = 0; link < links.getLength(); link++) {
+					((EventTarget) links.item(link)).addEventListener("click", new onClick(), false);
+				 }
+			}
+		}
+}
